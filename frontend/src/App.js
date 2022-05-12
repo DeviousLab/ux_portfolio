6a93fa021425c7128255b93ba9c8185af8306398
DeviousLab <deviouslab@gmail.com>
2022-05-12 21:31:01 +0400

    Setup file/folder structure
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 
+import { About, Footer, Header, Skills, Works, Testimonials } from './containers';
+import Navbar from './components/Navbar/Navbar';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-      Test
+    <div className='App'>
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Works />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
